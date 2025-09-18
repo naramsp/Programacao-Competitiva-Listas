b398bee84a48c0736c019e8eb05a862d0e780460
nara <5688.narapontes.cmr@gmail.com>
2025-09-18 11:38:51 -0300

    Só o arquivo inicial da questão B
    B - Destruction of the Dandelion Fields

---
+++ LE1/B/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*notas:
+
+1. a maquina começa desligada
+2. numeros impares MUDAM o estado(nao apenas ligam nem apenas desligam)
+3. a melhor soluçao sempre sera (eu acho): ligar no maior impar (e logo depois coletar esse impar),
+colher todos os pares, desligar no menor impar, ligar no segundo maior impar, desligar no segundo menor impar, ate terminar os campos
+*/
+
+int main(){
+    iostream::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    short testcases;
+    int campos;
+
+    int* impares = new int[campos];
+    int* pares = new int[campos];
+    
+    cout << "nao terminei ainda";
+
+    return 0;
+};
